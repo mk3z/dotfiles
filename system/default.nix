29acb5869ebd8289893487e2ae619f748c6959a4
Matias Zwinger <matias.zwinger@protonmail.com>
2023-08-26 12:51:44 +0300

    Reorganize and add new package manager settings

--- system/default.nix
+++ system/default.nix
@@ -87,17 +87,34 @@
     passwordFile = config.age.secrets.password.path;
   };
 
-  nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  nix = {
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 14d";
+    };
+
+    # Make `nix run nixpkgs#nixpkgs` use the same nixpkgs as the one used by this flake.
+    registry.nixpkgs.flake = inputs.nixpkgs;
+
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+      substituters = [ "https://nix-community.cachix.org" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+
   };
+
+  # Make `nix repl '<nixpkgs>'` use the same nixpkgs as the one used by this flake.
+  environment.etc."nix/inputs/nixpkgs".source = "${inputs.nixpkgs}";
+  nix.nixPath = [ "/etc/nix/inputs" ];
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  nix.settings.auto-optimise-store = true;
 
   # System packages
   environment.systemPackages = with pkgs;
@@ -151,6 +168,4 @@
   };
 
   sound.mediaKeys.enable = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
