47df641757758eda4372cf23186966abcc0c1470
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-02 13:19:59 +0300

    Revert to previous cursors

--- user/gui/cursor.nix
+++ user/gui/cursor.nix
@@ -1,14 +1,13 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   # I REALLY need to package this:
   # https://github.com/simtrami/posy-improved-cursor-linux
   home.pointerCursor = {
-    package = pkgs.capitaine-cursors-themed;
-    name = "Capitaine Cursors (Nord)";
+    package = pkgs.phinger-cursors;
+    name = "phinger-cursors";
     gtk.enable = true;
     x11 = {
       enable = true;
-      defaultCursor = "Capitaine Cursors (Nord)";
+      defaultCursor = "phinger-cursors";
     };
   };
 }
