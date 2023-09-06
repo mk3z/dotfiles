47fb943c0d1975e783f7e0c6b9471d5148edbc5a
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-06 13:25:39 +0300

    Remove unneeded config from doom

--- user/doom/doom/init.el
+++ user/doom/doom/init.el
@@ -112,7 +112,7 @@
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
-       (:if IS-MAC macos)  ; improve compatibility with macOS
+       ;;(:if IS-MAC macos); improve compatibility with macOS
        (tty +osc)          ; improve the terminal Emacs experience
 
        :lang
--- user/doom/doom/packages.el
+++ user/doom/doom/packages.el
@@ -66,11 +66,3 @@
 (package! org-fragtog)
 (package! rainbow-mode)
 (package! writeroom-mode)
-
-;; Temporary fix for magit
-(package! transient
-      :pin "c2bdf7e12c530eb85476d3aef317eb2941ab9440"
-      :recipe (:host github :repo "magit/transient"))
-(package! with-editor
-          :pin "bbc60f68ac190f02da8a100b6fb67cf1c27c53ab"
-          :recipe (:host github :repo "magit/with-editor"))
