03737ec0c016df04e9c3d1ee92a2e583df45a7aa
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-10 21:16:27 +0300

    Remap zathura binds

--- user/gui/zathura.nix
+++ user/gui/zathura.nix
@@ -1,1 +1,15 @@
-{ programs.zathura.enable = true; }
+{
+  programs.zathura = {
+    enable = true;
+    mappings = {
+      "m" = "scroll left";
+      "n" = "scroll down";
+      "e" = "scroll up";
+      "i" = "scroll right";
+      "u" = "recolor";
+    };
+    options = {
+      "selection-clipboard" = "clipboard";
+    };
+  };
+}
