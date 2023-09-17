6fadd155f8b18613bee0d33b7d871f38e26e6b8d
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-17 20:44:59 +0300

    Add cache.nixos.org as a substituter for hm

--- user/default.nix
+++ user/default.nix
@@ -68,11 +68,16 @@ in
     stateVersion = "23.11";
   };
 
-  nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
+  nix = {
+    registry.nixpkgs.flake = inputs.nixpkgs;
+    settings = {
+      # Idk why but for some reason hm does not put cache.nixos.org in nix.conf by default
+      substituters = [ "https://cache.nixos.org" "https://nix-community.cachix.org" ];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
   };
 
   programs = {
