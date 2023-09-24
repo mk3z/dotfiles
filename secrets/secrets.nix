7e84a0d356955a16d470894f03e1b72ad577d7a2
Matias Zwinger <matias.zwinger@protonmail.com>
2023-09-24 15:34:01 +0300

    Add borg backup service

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,4 +2,7 @@ let
   system =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDBntTB+Xhui/IciIGZW78dwlKxljidVH+q8FRDQQYA";
 in
-{ "password.age".publicKeys = [ system ]; }
+{
+  "password.age".publicKeys = [ system ];
+  "borg.age".publicKeys = [ system ];
+}
