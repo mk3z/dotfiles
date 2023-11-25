1a5bf1c79c08739ce1c71bfcd3022627f8243a1e
Matias Zwinger <matias.zwinger@protonmail.com>
2023-11-25 16:44:51 +0200

    Clean up cursor.nix

--- user/gui/cursor.nix
+++ user/gui/cursor.nix
@@ -1,13 +1,9 @@
 { pkgs, ... }: {
   # I REALLY need to package this:
   # https://github.com/simtrami/posy-improved-cursor-linux
-  home.pointerCursor = {
+
+  stylix.cursor = {
     package = pkgs.phinger-cursors;
     name = "phinger-cursors";
-    gtk.enable = true;
-    x11 = {
-      enable = true;
-      defaultCursor = "phinger-cursors";
-    };
   };
 }
