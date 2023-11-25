eb3806f2cfa4f0b76d1af549c4f1c82b396d142e
Matias Zwinger <matias.zwinger@protonmail.com>
2023-11-25 16:48:25 +0200

    Make codium usable

--- user/programs/vscode.nix
+++ user/programs/vscode.nix
@@ -12,27 +12,63 @@
       bbenoist.nix
       github.copilot
       kamadorueda.alejandra
+      scalameta.metals
       vscodevim.vim
     ];
     userSettings = {
       "editor.inlineSuggest.enabled" = true;
+      "editor.fontLigatures" = true;
+
       "security.workspace.trust.untrustedFiles" = "open";
+
       "window.menuBarVisibility" = "hidden";
+      "window.zoomLevel" = 2;
       "workbench.startupEditor" = "none";
       "workbench.colorTheme" = "Nord";
+
+      "keyboard.dispatch" = "keyCode";
+
+      "vim.leader" = " ";
+      "vim.vimrc.enable" = true;
+      "vim.useSystemClipboard" = true;
+
+      "files.watcherExclude" = {
+        "**/.bloop" = true;
+        "**/.metals" = true;
+        "**/.ammonite" = true;
+      };
     };
   };
 
-  # Tell VS Code to use Wayland
-  home.file.".vscode-oss/argv.json" = {
-    recursive = true;
-    text = ''
-      {
-        "enable-features": "UseOzonePlatform",
-        "ozone-platform" : "wayland",
-        "password-store":"gnome"
-      }
+  home.file = {
+    ".vimrc".text = ''
+      noremap e k
+      noremap i l
+      noremap k n
+      noremap l u
+      noremap m h
+      noremap n j
+      noremap u i
+      noremap E K
+      noremap I L
+      noremap K N
+      noremap L U
+      noremap M H
+      noremap N J
+      noremap U I
     '';
+
+    # Tell VS Code to use Wayland
+    ".vscode-oss/argv.json" = {
+      recursive = true;
+      text = ''
+        {
+          "enable-features": "UseOzonePlatform",
+          "ozone-platform": "wayland",
+          "password-store": "gnome"
+        }
+      '';
+    };
   };
 
   stylix.targets.vscode.enable = false;
