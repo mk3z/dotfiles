446bcb11807c921c9f2f8551db85abd3baeba3c3
Matias Zwinger <matias.zwinger@protonmail.com>
2023-12-02 14:37:37 +0200

    Add devenv cache

--- user/default.nix
+++ user/default.nix
@@ -63,12 +63,16 @@
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
       ];
-      extra-substituters = [
-        "https://crane.cachix.org"
-      ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+      extra-substituters = [
+        "https://devenv.cachix.org"
+        "https://crane.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
         "crane.cachix.org-1:8Scfpmn9w+hGdXH/Q9tTLiYAE/2dnJYRJP7kl80GuRk="
       ];
     };
