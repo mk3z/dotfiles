327315f544c228b78801252c8ad3d536cc43d67c
Matias Zwinger <matias.zwinger@protonmail.com>
2023-12-02 13:06:07 +0200

    Add CPU and MEM modules to waybar

--- user/gui/waybar.nix
+++ user/gui/waybar.nix
@@ -11,6 +11,8 @@
         height = 20;
         modules-left = [ "hyprland/workspaces" "hyprland/window" ];
         modules-right = [
+          "cpu"
+          "memory"
           "bluetooth"
           "network"
           "custom/vpn"
@@ -26,8 +28,9 @@
 
         "battery" = {
           interval = 10;
+          format-time = "{H}:{M}";
           format-icons = [ "󰂎" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
-          format = "{icon} {capacity}% {time} {power}W";
+          format = "{icon} {capacity}% {time} {power:0.2f}W";
         };
 
         "wireplumber" = {
@@ -35,6 +38,14 @@
           format = "{icon} {volume}%";
           format-muted = "󰝟";
           format-icons = [ "󰕿" "󰖀" "󰕾" ];
+        };
+
+        "bluetooth" = {
+          interval = 10;
+          format = "󰂯 {status}";
+          format-connecte = "󰂯 {device_alias}";
+          format-connected-battery =
+            "󰥈 {device_alias} {device_battery_percentage}%";
         };
 
         "custom/vpn" = {
@@ -47,18 +58,21 @@
 
         "network" = {
           interval = 10;
-          format-ethernet = "󰈁 {ipaddr}/{cidr}  {bandwidthUpBits}  {bandwidthDownBits}";
+          format-ethernet =
+            "󰈁 {ipaddr}/{cidr}  {bandwidthUpBits}  {bandwidthDownBits}";
           format-wifi =
             "󰖩 {essid} {frequency}GHz {signalStrength}%  {bandwidthUpBits}  {bandwidthDownBits}";
           format-disconnected = "disconnected";
         };
 
-        "bluetooth" = {
-          interval = 10;
-          format = "󰂯 {status}";
-          format-connecte = "󰂯 {device_alias}";
-          format-connected-battery =
-            "󰥈 {device_alias} {device_battery_percentage}%";
+        "memory" = {
+          interval = 5;
+          format = " {percentage}% 󰾴 {swapPercentage}%";
+        };
+
+        "cpu" = {
+          interval = 5;
+          format = " {usage}%";
         };
 
       };
@@ -79,7 +93,7 @@
         margin: 0;
       }
 
-      #clock, #battery, #wireplumber, #network, #custom-vpn, #bluetooth {
+      #clock, #battery, #wireplumber, #network, #custom-vpn, #bluetooth, #cpu, #memory {
         margin: 0 4px;
         padding: 0 4px;
         border-bottom: 1px solid;
