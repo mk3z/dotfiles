dff87218bd7b9bbc8a0274a2954ae40e7a5a3d58
Matias Zwinger <matias.zwinger@protonmail.com>
2023-12-02 13:04:26 +0200

    Test new keyboard layout

--- user/gui/keyboard.nix
+++ user/gui/keyboard.nix
@@ -14,6 +14,18 @@
           include "us(colemak_dh_iso)"
           replace key <AB05> { [ BackSpace ] };
           replace key <AC12> { [ backslash, bar ] };
+
+          key <AC03> { [ s, S, parenleft ] };
+          key <AC04> { [ t, T, parenright ] };
+
+          key <AC05> { [ g, G, less ] };
+          key <AC06> { [ m, M, greater ] };
+
+          key <AC07> { [ n, N, braceleft ] };
+          key <AC08> { [ e, E, braceright ] };
+
+          key <AB03> { [ d, D, bracketleft] };
+          key <AB07> { [ h, H, bracketright ] };
         };
       '';
     };
