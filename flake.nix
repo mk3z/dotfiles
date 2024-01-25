006ee926be0ea06279e01eae4bcdee848e9c1169
Matias Zwinger <matias.zwinger@protonmail.com>
2024-01-25 13:05:10 +0200

    Move host configuration to its own file

--- flake.nix
+++ flake.nix
@@ -2,82 +2,17 @@
   description = "mkez NixOS configuration";
 
   outputs = {
-    nixpkgs,
     flake-parts,
     devenv,
     ...
-  } @ inputs: let
-    # username needs to be defined here because it is used in user and system config
-    username = "matias";
-    homeDirectory = "/home/${username}";
-  in
+  } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
 
       imports = [
+        ./configurations.nix
         inputs.devenv.flakeModule
       ];
-
-      flake.nixosConfigurations = {
-        slimbook = let
-          inherit (inputs.nixpkgs.lib) nixosSystem;
-          homePersistDir = "/persist";
-        in
-          nixosSystem {
-            modules = [
-              ./hosts/slimbook
-              ./modules/system
-
-              # external
-              inputs.home-manager.nixosModule
-              inputs.impermanence.nixosModule
-              inputs.agenix.nixosModules.default
-              inputs.stylix.nixosModules.stylix
-
-              # hardware
-              ./modules/laptop.nix
-              ./modules/amd.nix
-              ./modules/amdgpu.nix
-              ./modules/zfs.nix
-              ./modules/bluetooth.nix
-
-              # features
-              ./modules/borg.nix
-              ./modules/sound.nix
-              ./modules/fonts.nix
-              ./modules/keyring.nix
-              ./modules/libvirt.nix
-              ./modules/man.nix
-              ./modules/ratbag.nix
-              ./modules/theme.nix
-
-              # programs
-              ./modules/docker.nix
-              ./modules/greetd.nix
-              ./modules/mullvad.nix
-              ./modules/podman.nix
-              ./modules/steam.nix
-              ./modules/syncthing.nix
-
-              {
-                home-manager = {
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  extraSpecialArgs = {
-                    inherit inputs username homeDirectory homePersistDir;
-                  };
-                  users.${username} = import ./user;
-                };
-              }
-            ];
-
-            specialArgs = {
-              inherit inputs;
-              inherit username homeDirectory homePersistDir;
-              sysPersistDir = "/persist";
-            };
-          };
-      };
 
       perSystem = {pkgs, ...}: {
         devenv.shells.default = {
