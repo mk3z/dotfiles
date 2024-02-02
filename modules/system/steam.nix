5210cdbfb62148b8cbfb1ebffd48fcc2316ae787
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-02 23:27:57 +0200

    Add gamemode

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   username,
   homePersistDir,
   homeDirectory,
@@ -11,7 +12,17 @@
 in {
   options.mkez.programs.steam.enable = mkEnableOption "Enable Steam";
   config = mkIf cfg.enable {
-    programs.steam.enable = true;
+    programs = {
+      steam.enable = true;
+      gamemode = {
+        enable = true;
+        enableRenice = true;
+        settings.custom = {
+          start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+          end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+        };
+      };
+    };
     environment.persistence."${homePersistDir}".directories = [
       {
         directory = "${homeDirectory}/.local/share/Steam";
