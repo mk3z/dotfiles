33b702e080c339771f5087147736bd80262d573d
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-04 16:08:45 +0200

    Add desktop ssh key to agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
+  desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyDZDc620uBfzn7J0WdlfTodFBib4aCsg1bo7GdOJrv";
   slimbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDBntTB+Xhui/IciIGZW78dwlKxljidVH+q8FRDQQYA";
+  workstations = [desktop slimbook];
 in {
-  "password.age".publicKeys = [slimbook];
-  "borg.age".publicKeys = [slimbook];
+  "password.age".publicKeys = workstations;
+  "borg.age".publicKeys = workstations;
 }
