c3c453da7a4f17f97fa91486a0fb3cce8716ccc3
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-09 18:15:42 +0200

    Add sioyek custom color scheme and shortcut

--- modules/user/programs/sioyek.nix
+++ modules/user/programs/sioyek.nix
@@ -1,6 +1,28 @@
 {
   programs.sioyek = {
     enable = true;
+    config = {
+      # base16-sioyek (https://github.com/loiccoyle/base16-sioyek)
+      # by Loic Coyle
+      # Nord scheme byarcticicestudio
+
+      custom_background_color = "#2e3440";
+      custom_text_color = "#eceff4";
+
+      page_separator_color = "#2e3440";
+      search_highlight_color = "#ebcb8b";
+      status_bar_color = "#2e3440";
+      status_bar_text_color = "#eceff4";
+      ui_text_color = "#eceff4";
+      ui_selected_text_color = "#eceff4";
+      ui_background_color = "#3b4252";
+      ui_selected_background_color = "#4c566a";
+      background_color = "#2e3440";
+      visual_mark_color = "0.29803923 0.3372549 0.41568628 0.2";
+      text_highlight_color = "#4c566a";
+      link_highlight_color = "#81a1c1";
+      synctex_highlight_color = "#bf616a";
+    };
     bindings = {
       "move_right" = "m";
       "move_down" = "n";
@@ -9,7 +31,7 @@
       "next_item" = "k";
       "previous_item" = "K";
 
-      "toggle_dark_mode" = "u";
+      "toggle_custom_color" = "u";
     };
   };
 
