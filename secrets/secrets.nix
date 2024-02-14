8b996e74914f2b0c407887a3b454967e48edf6d7
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-14 19:34:59 +0200

    Add memory-alpha ssh key to agenix and rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,8 +1,11 @@
 let
   desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyDZDc620uBfzn7J0WdlfTodFBib4aCsg1bo7GdOJrv";
   slimbook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBDBntTB+Xhui/IciIGZW78dwlKxljidVH+q8FRDQQYA";
+  memory-alpha = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZJWC/8EUEHQBw+g+XAoJuMC//URljkv5Nt+OegL8mW";
+
   workstations = [desktop slimbook];
+  servers = [memory-alpha];
 in {
-  "password.age".publicKeys = workstations;
+  "password.age".publicKeys = workstations ++ servers;
   "borg.age".publicKeys = workstations;
 }
