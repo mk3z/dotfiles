3df2792b5f86c6e25da61dee0ae53221239ef991
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-14 19:25:12 +0200

    Change stylix back to apply to servers too

--- modules/system/gui/theme.nix
+++ modules/system/gui/theme.nix
@@ -1,16 +1,7 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}: let
-  inherit (lib) mkIf;
-in {
-  config = mkIf (!config.mkez.core.server) {
-    stylix = {
-      image = ../../../images/wallpaper.jpg;
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
-      polarity = "dark";
-    };
+{pkgs, ...}: {
+  stylix = {
+    image = ../../../images/wallpaper.jpg;
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
+    polarity = "dark";
   };
 }
