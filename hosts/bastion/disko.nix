01754e97ba824d1373d1838ffeebd30d01fdc624
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-16 23:02:24 +0200

    More fighting with disko

--- hosts/bastion/disko.nix
+++ hosts/bastion/disko.nix
@@ -1,78 +1,80 @@
 {
-  disko = {
-    enableConfig = true;
-    devices.disk.sda = {
-      type = "disk";
-      device = "/dev/sda";
-
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            size = "512M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = ["defaults"];
-            };
-          };
-
-          crypt = {
-            size = "100%";
-            content = {
-              type = "luks";
-              name = "crypt";
-              extraOpenArgs = [];
-              settings = {
-                keyFile = "/tmp/keyfile";
-                allowDiscards = true;
-              };
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "500M";
+              type = "EF00";
               content = {
-                type = "lvm_pv";
-                vg = "pool";
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+
+            "/" = {
+              fsType = "tmpfs";
+              mountOptions = ["defaults" "size=4G" "nr_inodes=0" "mode=755"];
+            };
+
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                extraOpenArgs = [];
+                settings = {
+                  keyFile = "/tmp/keyfile";
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "lvm_pv";
+                  vg = "pool";
+                };
+              };
+            };
+
+            pool = {
+              type = "lvm_vg";
+              lvs = {
+                nix = {
+                  size = "20G";
+                  content = {
+                    type = "filesystem";
+                    format = "ext4";
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "defaults"
+                    ];
+                  };
+                };
+                swap = {
+                  size = "8G";
+                  content = {
+                    type = "swap";
+                  };
+                };
+                persist = {
+                  size = "100%";
+                  content = {
+                    type = "filesystem";
+                    format = "ext4";
+                    mountpoint = "/persist";
+                  };
+                };
               };
             };
           };
         };
       };
-    };
-
-    lvm_vg.pool = {
-      type = "lvm_vg";
-      lvs = {
-        nix = {
-          size = "20G";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/nix";
-            mountOptions = [
-              "defaults"
-            ];
-          };
-        };
-
-        swap = {
-          size = "8G";
-          content.type = "swap";
-        };
-
-        persist = {
-          size = "100%FREE";
-          content = {
-            type = "filesystem";
-            format = "ext4";
-            mountpoint = "/persist";
-          };
-        };
-      };
-    };
-
-    nodev."/" = {
-      fsType = "tmpfs";
-      mountOptions = ["defaults" "size=4G" "nr_inodes=0" "mode=755"];
     };
   };
 }
