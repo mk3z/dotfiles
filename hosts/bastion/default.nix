206df8d7b33e0309e485b78b7d62583f0829e79c
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-17 01:00:01 +0200

    bastion: Try static IP configuration

--- hosts/bastion/default.nix
+++ hosts/bastion/default.nix
@@ -11,6 +11,25 @@
   networking = {
     hostName = config.mkez.core.hostname;
     hostId = "c34261c9";
-    useDHCP = lib.mkDefault true;
+    useDHCP = false;
+    interfaces."enp1s0" = {
+      ipv4.addresses = [
+        {
+          address = "95.217.11.68";
+          prefixLength = 32;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2a01:4f9:c011:a051::1";
+          prefixLength = 64;
+        }
+      ];
+    };
+    defaultGateway = "95.217.11.68";
+    defaultGateway6 = {
+      address = "fe80::1";
+      interface = "enp1s0";
+    };
   };
 }
