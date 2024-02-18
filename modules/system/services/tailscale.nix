d405b22abf9a22247b0e94e2266a75879fe06f9d
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-18 19:29:05 +0200

    Stop mullvad from routing tailscale traffic

--- modules/system/services/tailscale.nix
+++ modules/system/services/tailscale.nix
@@ -11,8 +11,18 @@ in {
     services.tailscale.enable = true;
     networking.firewall = {
       checkReversePath = "loose";
-      trustedInterfaces = ["tailscale0"];
-      allowedUDPPorts = [config.services.tailscale.port];
+      trustedInterfaces = [config.services.tailscale.interfaceName];
     };
+
+    # Stop mullvad from routing tailscale traffic
+    # https://mullvad.net/en/help/split-tunneling-with-linux-advanced#excluding
+    networking.nftables.ruleset = mkIf config.services.mullvad-vpn.enable ''
+      table inet excludeTraffic {
+        chain excludeOutgoing {
+          type route hook output priority 0; policy accept;
+          ip daddr 100.64.0.0/10 ct mark set 0x00000f41 meta mark set 0x6d6f6c65;
+        }
+      }
+    '';
   };
 }
