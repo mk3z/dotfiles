0fe888094e4016b03523377d2ad3300b9c215167
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-20 15:22:34 +0200

    Don't permit SSH root login

--- modules/system/services/ssh.nix
+++ modules/system/services/ssh.nix
@@ -13,7 +13,10 @@ in {
     services.openssh = {
       enable = true;
       openFirewall = true;
-      settings.PasswordAuthentication = false;
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
     };
 
     users.users.${username}.openssh.authorizedKeys.keys = [
