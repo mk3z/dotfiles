9be4dba7029fba31181144047385fa16647c19d8
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-21 00:55:44 +0200

    Put jellyfin behind nginx reverse proxy

--- modules/system/services/jellyfin.nix
+++ modules/system/services/jellyfin.nix
@@ -5,12 +5,29 @@
 }: let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.mkez.services.jellyfin;
+  inherit (config.mkez.core) hostname;
+  inherit (config.services.tailscale) interfaceName;
 in {
   options.mkez.services.jellyfin.enable = mkEnableOption "Whether to enable Jellyfin";
   config = mkIf cfg.enable {
-    services.jellyfin = {
-      enable = true;
-      openFirewall = true;
+    services = {
+      jellyfin = {
+        enable = true;
+        openFirewall = true;
+      };
+
+      nginx = {
+        enable = true;
+
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+
+        virtualHosts."${hostname}.intra.mkez.fi".locations."/jellyfin".proxyPass = "http://localhost:8096";
+      };
     };
+
+    networking.firewall.interfaces.${interfaceName}.allowedTCPPorts = [80 443];
   };
 }
