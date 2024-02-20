b6f1ccbd2f88280d0de0c9b18fa539e8ba8804df
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-21 00:55:08 +0200

    Put arr stack behind nginx reverse proxy

--- modules/system/services/arr.nix
+++ modules/system/services/arr.nix
@@ -5,22 +5,34 @@
 }: let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.mkez.services.arr;
+  inherit (config.mkez.core) hostname;
+  inherit (config.services.tailscale) interfaceName;
 in {
   options.mkez.services.arr.enable = mkEnableOption "Whether to enable arr stack";
   config = mkIf cfg.enable {
     services = {
-      lidarr = {
+      lidarr.enable = true;
+      radarr.enable = true;
+      sonarr.enable = true;
+
+      nginx = {
         enable = true;
-        openFirewall = true;
-      };
-      radarr = {
-        enable = true;
-        openFirewall = true;
-      };
-      sonarr = {
-        enable = true;
-        openFirewall = true;
+
+        recommendedGzipSettings = true;
+        recommendedOptimisation = true;
+        recommendedProxySettings = true;
+        recommendedTlsSettings = true;
+
+        virtualHosts."${hostname}.intra.mkez.fi" = {
+          locations = {
+            "/lidarr".proxyPass = "http://localhost:8686";
+            "/radarr".proxyPass = "http://localhost:7878";
+            "/sonarr".proxyPass = "http://localhost:8989";
+          };
+        };
       };
     };
+
+    networking.firewall.interfaces.${interfaceName}.allowedTCPPorts = [80 443];
   };
 }
