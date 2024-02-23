c63f1b8c8ebd098d5c084ae7885d4a498aafe9c1
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-23 03:13:32 +0200

    Add unstable option to zfs module

--- modules/system/hardware/zfs.nix
+++ modules/system/hardware/zfs.nix
@@ -1,29 +1,31 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) types mkOption mkEnableOption mkIf;
   cfg = config.mkez.hardware.zfs;
 in {
-  options.mkez.hardware.zfs.enable = mkEnableOption "Enable ZFS support";
+  options.mkez.hardware.zfs = {
+    enable = mkEnableOption "Enable ZFS support";
+    unstable = mkOption {
+      description = "Whether to use unstable ZFS";
+      type = types.bool;
+      default = true;
+    };
+  };
   config = mkIf cfg.enable {
     services.zfs.autoScrub.enable = true;
 
     boot = {
       supportedFilesystems = ["zfs"];
-      kernelPackages =
-        (pkgs.zfsUnstable.override {
-          removeLinuxDRM = pkgs.hostPlatform.isAarch64;
-        })
-        .latestCompatibleLinuxPackages;
+      kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
       kernelParams = ["nohibernate"];
 
       zfs = {
         forceImportRoot = false;
         removeLinuxDRM = true;
-        enableUnstable = true;
+        enableUnstable = cfg.unstable;
       };
     };
   };
