1368f45e36f3a6fcb2c5b89e414e14dd59c9b3b6
Matias Zwinger <matias.zwinger@protonmail.com>
2024-02-26 14:40:54 +0200

    Fix typo in nas powerUpCommands

--- hosts/nas/default.nix
+++ hosts/nas/default.nix
@@ -18,7 +18,7 @@
   systemd.services.zfs-mount.enable = false;
 
   powerManagement.powerUpCommands = ''
-    "${pkgs.hdparm}/sbin/hdparm -B 64 /dev/sd{a,b,c}"
-    "${pkgs.hdparm}/sbin/hdparm -S 120 /dev/sd{a,b,c}"
+    ${pkgs.hdparm}/sbin/hdparm -B 64 /dev/sd{a,b,c}
+    ${pkgs.hdparm}/sbin/hdparm -S 120 /dev/sd{a,b,c}
   '';
 }
