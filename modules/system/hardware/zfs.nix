992aa6bd47958005c240348825e8817f7c6120b4
Matias Zwinger <matias.zwinger@protonmail.com>
2024-04-15 20:13:51 +0300

    feat(zfs): tweak ZFS kernel module options

--- modules/system/hardware/zfs.nix
+++ modules/system/hardware/zfs.nix
@@ -30,6 +30,10 @@ in {
         forceImportRoot = false;
         package = mkIf cfg.unstable pkgs.zfs_unstable;
       };
+
+      extraModprobeConfig = ''
+        options zfs l2arc_rebuild_enabled=1 l2arc_write_boost=524288000 l2arc_write_max=524288000 zfs_txg_timeout=300 zfs_dirty_data_max=4174112768 zfs_dirty_data_max_percent=40 l2arc_headroom=12
+      '';
     };
   };
 }
