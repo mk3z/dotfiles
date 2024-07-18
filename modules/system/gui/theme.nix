9c01f8b52c69dd008c5127c20bb93440aece7bbe
Matias Zwinger <matias.zwinger@protonmail.com>
2024-07-18 17:53:09 +0300

    chore: update stylix

--- modules/system/gui/theme.nix
+++ modules/system/gui/theme.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   stylix = {
+    enable = true;
     image = ../../../images/wallpaper.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
     polarity = "dark";
