ea5eb7ac040f0bdc42e645722af844074ce07291
Matias Zwinger <matias.zwinger@protonmail.com>
2024-08-04 16:23:11 +0300

    fix: syncthing accidentally enabled for all servers

--- modules/system/services/syncthing.nix
+++ modules/system/services/syncthing.nix
@@ -9,18 +9,17 @@
   inherit (config.services.tailscale) interfaceName;
 in {
   options.mkez.services.syncthing.enable = mkEnableOption "Enable Syncthing";
-  config =
-    mkIf cfg.enable {
-      networking.firewall = {
-        allowedTCPPorts = [22000];
-        allowedUDPPorts = [22000 21027];
-      };
-    }
-    // mkIf config.mkez.core.server {
-      services.syncthing = {
-        enable = true;
-        guiAddress = "0.0.0.0:${toString guiPort}";
-      };
-      networking.firewall.interfaces.${interfaceName}.allowedTCPPorts = [guiPort];
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      guiAddress = "0.0.0.0:${toString guiPort}";
     };
+
+    networking.firewall = {
+      allowedTCPPorts = [22000];
+      allowedUDPPorts = [22000 21027];
+    };
+
+    networking.firewall.interfaces.${interfaceName}.allowedTCPPorts = mkIf config.mkez.core.server [guiPort];
+  };
 }
