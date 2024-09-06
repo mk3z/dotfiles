a4255445f5e1f07667d791a0645cb78233461412
Matias Zwinger <matias.zwinger@protonmail.com>
2024-09-06 23:55:17 +0300

    fix: run syncthing as the user on non-servers

--- modules/system/services/syncthing.nix
+++ modules/system/services/syncthing.nix
@@ -7,12 +7,23 @@
   cfg = config.mkez.services.syncthing;
   guiPort = 8384;
   inherit (config.services.tailscale) interfaceName;
+  inherit (config.mkez.core) server;
+  inherit (config.mkez.user) username;
+  inherit (config.users.users."${config.mkez.user.username}") group;
 in {
   options.mkez.services.syncthing.enable = mkEnableOption "Enable Syncthing";
   config = mkIf cfg.enable {
     services.syncthing = {
       enable = true;
       guiAddress = "0.0.0.0:${toString guiPort}";
+      user =
+        if server
+        then "syncthing"
+        else username;
+      group =
+        if server
+        then "syncthing"
+        else group;
     };
 
     networking.firewall = {
