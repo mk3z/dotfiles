6588341d874ce4cc4b620231be3c968ef33782f9
Matias Zwinger <matias@zwinger.fi>
2024-09-17 20:18:40 +0300

    feat: PAM login with YubiKey

--- modules/system/yubikey.nix
+++ modules/system/yubikey.nix
@@ -6,10 +6,30 @@
 }: let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.mkez.features.yubikey;
+  inherit (config.mkez.core) homePersistDir;
+  inherit (config.mkez.user) username homeDirectory;
 in {
   options.mkez.features.yubikey.enable = mkEnableOption "Whether to enable Yubikey support";
+
   config = mkIf cfg.enable {
-    services.udev.packages = [pkgs.yubikey-personalization];
+    services = {
+      udev.packages = [pkgs.yubikey-personalization];
+      pcscd.enable = true;
+    };
+
+    security.pam.services = {
+      login.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
+
     environment.systemPackages = with pkgs; [yubikey-manager];
+
+    environment.persistence."${homePersistDir}".directories = [
+      {
+        directory = "${homeDirectory}/.config/Yubico";
+        user = username;
+        group = "users";
+      }
+    ];
   };
 }
