4d6aa0778d524291cd8fe5b2735ba479dfcc3688
Matias Zwinger <matias@zwinger.fi>
2024-10-12 20:00:28 +0300

    fix(rtl-sdr): add user to plugdev group

--- modules/system/hardware/rtl-sdr.nix
+++ modules/system/hardware/rtl-sdr.nix
@@ -4,11 +4,13 @@
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
+  inherit (config.mkez.user) username;
   cfg = config.mkez.hardware.rtl-sdr;
 in {
-  options.mkez.programs.rtl-sdr.enable = mkEnableOption "Whether to enable RTL-SDR hardware support";
+  options.mkez.hardware.rtl-sdr.enable = mkEnableOption "Whether to enable RTL-SDR hardware support";
   config = mkIf cfg.enable {
     hardware.rtl-sdr.enable = true;
     services.sdrplayApi.enable = true;
+    users.users.${username}.extraGroups = ["plugdev"];
   };
 }
