c82454bacf5f2c7ed162cfccbf8d09e1ee3ab131
Matias Zwinger <matias@zwinger.fi>
2024-11-08 20:29:22 +0200

    feat(docker): add rootless docker

--- modules/system/virtualisation/docker.nix
+++ modules/system/virtualisation/docker.nix
@@ -10,7 +10,15 @@
 in {
   options.mkez.virtualisation.docker.enable = mkEnableOption "Enable Docker";
   config = mkIf cfg.enable {
-    virtualisation.docker.enable = true;
+    virtualisation.docker = {
+      enable = true;
+      autoPrune.enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+      storageDriver = "overlay2";
+    };
     environment.systemPackages = with pkgs; [docker-compose];
     users.users.${username}.extraGroups = ["docker"];
   };
