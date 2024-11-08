18be5596325aa626a898b5e79d61ea2e42f5f144
Matias Zwinger <matias@zwinger.fi>
2024-11-08 20:54:37 +0200

    fix: disable colemak in vconsole for servers

--- modules/system/common/console.nix
+++ modules/system/common/console.nix
@@ -1,4 +1,10 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (config.mkez.core) server;
+in {
   # Some basic system maintenance packages
   environment.systemPackages = with pkgs; [
     htop
@@ -14,6 +20,12 @@
   };
 
   # Console keymap
-  console.packages = with pkgs; [colemak-dh];
-  console.keyMap = "colemak_dh_iso_us";
+  console =
+    if server
+    then {}
+    # Server should have US layout for ease of operation over online KVM inferfaces.
+    else {
+      packages = with pkgs; [colemak-dh];
+      keyMap = "colemak_dh_iso_us";
+    };
 }
