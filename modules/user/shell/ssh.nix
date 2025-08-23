dd9dabdf324e35e2bb248eeaa9dab345ee852f4d
Matias Zwinger <matias@zwinger.fi>
2025-08-23 13:28:27 +0300

    feat(ssh): kerberos support

--- modules/user/shell/ssh.nix
+++ modules/user/shell/ssh.nix
@@ -1,25 +1,44 @@
 {
+  lib,
   osConfig,
   pkgs,
   ...
 }: let
   inherit (osConfig.mkez.core) homePersistDir;
   inherit (osConfig.mkez.user) homeDirectory;
+  package =
+    if osConfig.mkez.services.krb.enable
+    then pkgs.openssh_gssapi.override {withKerberos = osConfig.mkez.services.krb.enable;}
+    else pkgs.openssh;
 in {
   home = {
     persistence."${homePersistDir}${homeDirectory}".directories = [".ssh"];
-    packages = [pkgs.mosh];
+    packages = [
+      (pkgs.mosh.override {openssh = package;})
+      pkgs.sshfs
+    ];
   };
 
   programs.ssh = {
     enable = true;
+    inherit package;
     addKeysToAgent = "yes";
     matchBlocks = {
       nas = {
         forwardAgent = true;
       };
+      "lxplus*" = {
+        user = "mzwinger";
+        extraOptions = {
+          GSSAPIAuthentication = "yes";
+          GSSAPIDelegateCredentials = "yes";
+          GSSAPITrustDns = "yes";
+        };
+      };
     };
   };
+
+  programs.git.package = lib.mkForce (pkgs.git.override {openssh = package;});
 
   services.ssh-agent.enable = true;
 }
